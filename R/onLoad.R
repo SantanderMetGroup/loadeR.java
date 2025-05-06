b4b963294a646845a9b8a800add2e345cfe749f6
Antonio S. Cofiño <cofinoa@gmail.com>
2025-05-06 17:59:07 +0200

    Refactor JVM memory setting in .onLoad function to check initialization status

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,12 +1,15 @@
 #' @importFrom rJava .jpackage
 
 .onLoad <- function(libname, pkgname) {
-  # Set the maximum memory for the Java Virtual Machine (JVM)
-  options(java.parameters = "-Xmx2g")
-  
+  if (!rJava::.jniInitialized) {
+    options(java.parameters = "-Xmx2g")
+  } else {
+    warning("JVM is already initialized; java.parameters could not be set.")
+  }
+
   # Get the correct path to the 'java' directory inside the package
   java_path <- system.file("java", package = pkgname)
-  
+
   # Initialize rJava and add all JARs in the 'java' directory to the classpath
   rJava::.jpackage(pkgname, lib.loc = dirname(java_path))
 }
