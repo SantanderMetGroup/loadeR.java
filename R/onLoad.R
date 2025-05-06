f1197929caa9999f01615b888defe7f628d3356d
Antonio S. Cofiño <cofinoa@gmail.com>
2025-05-06 16:12:32 +0200

    Update onLoad.R

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,6 +1,12 @@
 #' @importFrom rJava .jpackage
 
-.onLoad <- function(libname, pkgname = "netcdfAll-4.6.0-SNAPSHOT.jar") {
-      .jpackage(pkgname, lib.loc = libname)
-      options(java.parameters = "-Xmx2g")
+.onLoad <- function(libname, pkgname) {
+  # Set the maximum memory for the Java Virtual Machine (JVM)
+  options(java.parameters = "-Xmx2g")
+  
+  # Get the correct path to the 'java' directory inside the package
+  java_path <- system.file("java", package = pkgname)
+  
+  # Initialize rJava and add all JARs in the 'java' directory to the classpath
+  rJava::.jpackage(pkgname, lib.loc = dirname(java_path))
 }
